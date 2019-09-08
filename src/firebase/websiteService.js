8f6ad617fc6914d80969d3308ed9c5405e66bbe7
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-09-08 08:38:48 -0400

    add regex

--- src/firebase/websiteService.js
+++ src/firebase/websiteService.js
@@ -1,6 +1,10 @@
 import { websitesRef } from './firebase';
 
 export const addWebsite = website => {
+	const url = website.url;
+	if (url[url.length - 1] === '/') { website.url = url + '*/'; }
+	else { website.url = url + '/*/' }
+	console.log(website);
 	websitesRef.push(website);
 }
 
